5ba944d7b202b75293aebc635271f0ecf6c4ad86
Jesper Rønn-Jensen <jesperrr@gmail.com>
2014-11-13 10:12:24 +0100

    vores modificerede test case

--- src/js/util/code.js
+++ src/js/util/code.js
@@ -2,4 +2,11 @@
     'use strict';
 
     window.add = function (a, b) { return a + b; };
+    window.sum = function () {
+    	var sum = 0;
+    	for(var i = 0; i < arguments.length; i++) {
+    		sum += arguments[i];
+    	}
+    	return sum;
+    };
 })(window);
--- test/js/util/code.test.js
+++ test/js/util/code.test.js
@@ -5,6 +5,38 @@ describe('util.add', function () {
 
     it('should sum', function () {
         expect(add(2, 3)).toBe(5);
+        expect(add(4, 3)).toBe(7);
     });
 
+    it('should sum negatively', function () {
+        expect(add(-2, -3)).toBe(-5);
+        expect(add(-4, 3)).toBe(-1);
+    });
+
+
 });
+
+describe('util.sum', function(){
+	var sum = window.sum;
+
+	it('should sum two arguments', function() {
+		expect(sum(2,3)).toBe(5);
+	})
+
+	it('should sum many arguments', function() {
+		expect(sum(1,2,3,4,5,6)).toBe(21);
+	})
+
+	it('should return 0 for zero arguments', function() {
+		expect(sum()).toBe(0);
+	})
+
+	it('should return number for number and number-ish string', function() {
+		expect(sum(2, '2')).toBe(4);
+	})
+
+	it('should return number for number and number-ish string', function() {
+		expect(sum(2, 3, 'not a number')).toBe(5);
+	})
+})
+
